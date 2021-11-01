a986f0afedfcc6f1d545b981fe78cdfb2649cf64
Emmanuel Lion <55059322+Mabule@users.noreply.github.com>
2021-11-01 23:30:04 +0100

    v0.1

--- js/app.js
+++ js/app.js
@@ -1,13 +1,45 @@
-import anime from '../node_modules/animejs/lib/anime.es.js';
-
-var elements = document.querySelectorAll('#name');
-
-anime({
-  targets: '#name',
-  strokeDashoffset: [anime.setDashoffset, 0],
-  easing: 'easeInOutSine',
-  duration: 1500,
-  delay: function(el, i) { return i * 250 },
-  direction: 'alternate',
-  loop: true
+let size = 2;
+let d = {
+		'x': 0,
+		'y': 0,
+		'z': 0,
+		'a': new Array(size)
+};
+d['a'].forEach(el => {
+	el = {
+		'x': 0,
+		'y': 0,
+		'z': 0,
+		'a': new Array(size)
+	};
+	el['a'].forEach(le => {
+		le = {
+			'x': 0,
+			'y': 0,
+			'z': 0,
+			'a': new Array(size)
+		};
+	});
 });
+d['a'].forEach(el => {
+    console.log(typeof(el));
+    el['a'].forEach(le => {
+        console.log(le);
+        le['a'].forEach(e => {
+            console.log(e);
+        })
+    })
+})
+
+function load(){
+	let blob1 = document.getElementById("blob_presentation");
+	let blob2 = document.getElementById("blob_link_history");
+	blob1.style.top = "-100px";
+	blob1.style.left = "-158px";
+	blob1.style.transform = "rotateZ(360deg)";
+
+	blob2.style.top = "-140px";
+	blob2.style.left = "-95px";
+	blob2.style.transform = "rotateZ(360deg)";
+}
+
